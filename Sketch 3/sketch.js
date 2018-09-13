0e0e817a17b44e535281f9f6317146dbd7325002
Adam Tindale <adam@adamtindale.com>
2018-09-13 12:00:40 -0400

    sketch 3 plays sounds now

--- Sketch 3/sketch.js
+++ Sketch 3/sketch.js
@@ -1,6 +1,8 @@
+var mysound;
 
 function setup(){
 	createCanvas(720, 480);
+	mysound = createAudio("industry_mad.wav");
 }
 
 function draw(){
@@ -11,5 +13,6 @@ function mouseClicked(){
 	var clickdistance = dist(width/2, height/2, mouseX, mouseY);
 	if(clickdistance < 15){
 		fill(100);
+		mysound.play();
 	}
 }
