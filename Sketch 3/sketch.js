2c1c7404a5d6cff48a0c0561fbbe6038dfc0d6f6
Adam Tindale <adam@adamtindale.com>
2018-09-13 12:11:02 -0400

    put bubble back to middle when it leaves the screen

--- Sketch 3/sketch.js
+++ Sketch 3/sketch.js
@@ -1,17 +1,33 @@
 var mysound;
+var bubble; 
 
 function setup(){
 	createCanvas(720, 480);
 	mysound = createAudio("industry_mad.wav");
+	bubble = {
+		x: width/2,
+		y: height/2,
+		r: 15
+	};
 }
 
 function draw(){
-	ellipse(width/2, height/2, 30);
+	ellipse(bubble.x, bubble.y, bubble.r * 2);
+	bubble.x = bubble.x + random(-5,5);
+	bubble.y = bubble.y + random(-5,5);
+	if(bubble.x > width || bubble.x < 0){
+		bubble.y = height/2;
+		bubble.x = width/2;
+	}
+	if(bubble.y > height || bubble.y < 0){
+		bubble.y = height/2;
+		bubble.x = width/2;
+	}
 }
 
 function mouseClicked(){
-	var clickdistance = dist(width/2, height/2, mouseX, mouseY);
-	if(clickdistance < 15){
+	var clickdistance = dist(bubble.x, bubble.y, mouseX, mouseY);
+	if(clickdistance < bubble.r){
 		fill(100);
 		mysound.play();
 	}
