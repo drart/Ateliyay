04424978a2d7b7c9fede6b6f8f03a56b270dc837
Adam Tindale <adam@adamtindale.com>
2018-09-13 10:53:33 -0400

    mouse click bad particle system

--- Sketch 2/sketch.js
+++ Sketch 2/sketch.js
@@ -8,15 +8,16 @@ function setup(){
 function draw(){
 	for(var i = 0; i < mouseclicks.length; i++){
 		ellipse(mouseclicks[i].x, mouseclicks[i].y, 10);
-		mouseclicks[i].x++;
-		mouseclicks[i].y++;
+		mouseclicks[i].x = mouseclicks[i].x + mouseclicks[i].speed;
+		mouseclicks[i].y = mouseclicks[i].y + mouseclicks[i].speed;
 	}
 }
 
 function mouseClicked(){
 	var clickposition = {
 		x: mouseX,
-		y: mouseY
+		y: mouseY,
+		speed: random(-2,2)
 	}
 	mouseclicks.push(clickposition);
 }
