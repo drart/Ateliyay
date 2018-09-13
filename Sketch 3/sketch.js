0504d89c5a565e4c470e9c47765155197b0ed00b
Adam Tindale <adam@adamtindale.com>
2018-09-13 11:50:40 -0400

    circle collision detector

--- Sketch 3/sketch.js
+++ Sketch 3/sketch.js
@@ -4,5 +4,13 @@ function setup(){
 }
 
 function draw(){
-	
+	ellipse(width/2, height/2, 30);
+}
+
+function mouseClicked(){
+	if(mouseX < width/2 + 15 && mouseX > width/2 - 15){
+		if(mouseY < height/2 + 15 && mouseY > height/2 -15){
+			fill(100);
+		}
+	}
 }
