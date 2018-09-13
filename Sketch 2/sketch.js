6a693c457f7a67807054ae4bec01c41c04c396e0
Adam Tindale <adam@adamtindale.com>
2018-09-13 11:21:33 -0400

    added p5dom to sketch 2

--- Sketch 2/sketch.js
+++ Sketch 2/sketch.js
@@ -2,22 +2,24 @@ var mouseclicks = [];
 
 function setup(){
 	createCanvas(720,480);
+	var button = createButton('add particle');
 }
 
 
 function draw(){
 	for(var i = 0; i < mouseclicks.length; i++){
 		ellipse(mouseclicks[i].x, mouseclicks[i].y, 10);
-		mouseclicks[i].x = mouseclicks[i].x + mouseclicks[i].speed;
-		mouseclicks[i].y = mouseclicks[i].y + mouseclicks[i].speed;
+		mouseclicks[i].x = mouseclicks[i].x + mouseclicks[i].xspeed;
+		mouseclicks[i].y = mouseclicks[i].y + mouseclicks[i].yspeed;
 	}
 }
 
-function mouseClicked(){
+function addparticle(){
 	var clickposition = {
 		x: mouseX,
 		y: mouseY,
-		speed: random(-2,2)
+		xspeed: random(-2,2),
+		yspeed: random(-2,2)
 	}
 	mouseclicks.push(clickposition);
 }
