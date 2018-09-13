590b3376a04146178a1600c06a4a905d250becc4
Adam Tindale <adam@adamtindale.com>
2018-09-13 11:54:56 -0400

    hooray for trig!

--- Sketch 3/sketch.js
+++ Sketch 3/sketch.js
@@ -8,9 +8,8 @@ function draw(){
 }
 
 function mouseClicked(){
-	if(mouseX < width/2 + 15 && mouseX > width/2 - 15){
-		if(mouseY < height/2 + 15 && mouseY > height/2 -15){
-			fill(100);
-		}
+	var clickdistance = dist(width/2, height/2, mouseX, mouseY);
+	if(clickdistance < 15){
+		fill(100);
 	}
 }
